829693678b43df21ef05cedd52eb6fb8c916328e
Emanuele Greppi <68281069+xenical73@users.noreply.github.com>
2024-01-18 21:29:08 +0100

    Update SLY_Lab_Assistant.user.js

--- SLY_Lab_Assistant.user.js
+++ SLY_Lab_Assistant.user.js
@@ -6,9 +6,9 @@
 // @author       SLY w/ Contributions by SkyLove512, anthonyra, niofox
 // @match        https://*.labs.staratlas.com/
 // @require      https://unpkg.com/@solana/web3.js@latest/lib/index.iife.min.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/anchor-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/buffer-browserified.js
-// @require      https://raw.githubusercontent.com/ImGroovin/SAGE-Lab-Assistant/main/bs58-browserified.js
+// @require      https://raw.githubusercontent.com/xenical73/LabsAssistant-0.4.2/main/anchor-browserified.js
+// @require      https://raw.githubusercontent.com/xenical73/LabsAssistant-0.4.2/main/buffer-browserified.js
+// @require      https://raw.githubusercontent.com/xenical73/LabsAssistant-0.4.2/main/bs58-browserified.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=staratlas.com
 // @grant        GM_setValue
 // @grant        GM_getValue
@@ -20,7 +20,8 @@
 
     let enableAssistant = false;
     let initComplete = false;
-    let rpcEndpoints = ['https://rpc.hellomoon.io/cfd5910f-fb7d-4489-9b32-f97193eceefd','https://solana-api.syndica.io/access-token/WPoEqWQ2auQQY1zHRNGJyRBkvfOLqw58FqYucdYtmy8q9Z84MBWwqtfVf8jKhcFh/rpc']
+    let rpcEndpoints = ['https://solana-mainnet.g.alchemy.com/v2/WUx8VnwE37Rgv4-4Gd-uIZ9JdmN-Mzyt']
+    //let rpcEndpoints = ['https://rpc.hellomoon.io/cfd5910f-fb7d-4489-9b32-f97193eceefd','https://solana-api.syndica.io/access-token/WPoEqWQ2auQQY1zHRNGJyRBkvfOLqw58FqYucdYtmy8q9Z84MBWwqtfVf8jKhcFh/rpc']
 
     const connectionProxy = {
         get(target, key, receiver) {
@@ -728,7 +729,7 @@
 
             let latestBH = await solanaConnection.getLatestBlockhash('confirmed');
             tx.recentBlockhash = latestBH.blockhash;
-            tx.lastValidBlockHeight = latestBH.lastValidBlockHeight-150;
+            tx.lastValidBlockHeight = latestBH.lastValidBlockHeight-250;
             tx.feePayer = userPublicKey;
             tx.signer = userPublicKey;
             let txSigned = null;
